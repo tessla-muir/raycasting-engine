693f6adf8ec11d2ff6699adde1f6aa7ec4506628
Tessla Muir <tesslamuir@yahoo.com>
2024-04-20 21:47:22 -0400

    Simple SDL Window & Input Processing

--- raycasting-engine/raycasting-engine/main.c
+++ raycasting-engine/raycasting-engine/main.c
@@ -1,8 +1,88 @@
 #include <stdio.h>
 #include <SDL.h>
 
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+int isProgramRunning = 1;
+
+int InitalizeWindow() {
+	// Intializes everything for SDL
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "SDL: Init Intialization Error.\n");
+		return 0;
+	}
+
+	window = SDL_CreateWindow(
+		"740 Final Project", // Window title
+		SDL_WINDOWPOS_CENTERED, // Center the window on width & height
+		SDL_WINDOWPOS_CENTERED, 
+		800, // Window resolution: width & height
+		600,
+		SDL_WINDOW_SHOWN // Show window
+		);
+
+	// Ensure window intialization
+	if (!window) {
+		fprintf(stderr, "SDL: Window Intialization Error.\n");
+		return 0;
+	}
+
+	renderer = SDL_CreateRenderer(window, -1, 0);
+
+	// Ensure renderer intialization
+	if (!renderer) {
+		fprintf(stderr, "SDL: Renderer Intialization Error\n");
+		return 0;
+	}
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
+	return 1;
+}
+
+void DestroyWindow() {
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+void InputProcessing() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type) {
+		case SDL_QUIT: {
+			isProgramRunning = 0;
+			break;
+		}
+		case SDL_KEYDOWN: {
+			if (event.key.keysym.sym == SDLK_ESCAPE) {
+				isProgramRunning = 0;
+				break;
+			}
+		}
+	}
+}
+
+void Render() {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	// TODO:
+	// Should render all objects on the current cycle
+
+	SDL_RenderPresent(renderer);
+}
+
 int main(int argc, char* args[]) {
-	SDL_Init(SDL_INIT_EVERYTHING);
-	printf("Hello, world");
+	isProgramRunning = InitalizeWindow();
+
+	while (isProgramRunning)
+	{
+		InputProcessing();
+		Render();
+	}
+
+	DestroyWindow();
+
 	return 0;
 }
